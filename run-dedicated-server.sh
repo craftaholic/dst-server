0e6621d846b11b2e778b8fcd12a4daf75127d10e
= <=>
2025-06-29 15:05:51 +0000

    feat: adding mod features

--- run-dedicated-server.sh
+++ run-dedicated-server.sh
@@ -28,6 +28,21 @@ check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
 
 ./steamcmd.sh +force_install_dir $install_dir +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +app_update 343050 validate +quit
 
+# Configure mod:
+ds_mods_setup="$dontstarve_dir/$cluster_name/mods/dedicated_server_mods_setup.lua"
+if [ -f "$ds_mods_setup" ]
+then
+  cp $ds_mods_setup "$HOME/downloaded/mods/"
+fi
+
+# Copy modoverrides.lua
+modoverrides="$dontstarve_dir/$cluster_name/mods/modoverrides.lua"
+if [ -f "$modoverrides" ]
+then
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Master/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Caves/"
+fi
+
 check_for_file "$install_dir/bin"
 
 cd "$install_dir/bin" || fail
